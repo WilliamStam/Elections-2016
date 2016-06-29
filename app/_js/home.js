8268eecf6d9357a382b66ecd5ed21fb8ca244b98
William Stam <william@impreshin.com>
2016-06-30 01:12:52 +0200

    latest

--- app/_js/home.js
+++ app/_js/home.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
 	
 	
+$(".status-lookup-form").on("submit",function(e){
+	e.preventDefault();
+	$(".form-validation",this).remove();
+	var $IDNumber = $("#IDNumber",this);
+	var IDNumber = $IDNumber.val();
 	
+	if (IDNumber){
+		
+		$.get("/iec/voter/"+IDNumber,function(data){
+			$("#modal-window").jqotesub($("#template-voter-status"), data).modal("show").on("hidden.bs.modal", function () {
+				
+			})
+			
+		})
+		
+		
+		
+		
+	} else {
+		$field = $IDNumber.parent();
+		$field.addClass("has-error");
+		$field.after('<span class="help-block s form-validation">ID Number required</span>');
+	}
+	
+	
+})	
 	
 	
 });
